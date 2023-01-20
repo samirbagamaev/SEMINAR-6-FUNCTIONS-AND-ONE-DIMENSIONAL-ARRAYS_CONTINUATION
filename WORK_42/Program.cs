c96b30ef2b5a9cee201edfa29314a781453b0bc9
Samir Bagamaev <05ru4315@gmail.com>
2023-01-20 21:52:52 +0300

    WORK_42

---
+++ WORK_42/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string Change(int decimalNumber)
+{
+    var binaryNumber = string.Empty;
+    while (decimalNumber > 0)
+    {
+        binaryNumber = (decimalNumber % 2) + binaryNumber;
+        decimalNumber /= 2;
+    }
+    return binaryNumber;
+}
+
+Console.Write("ВВЕДИТЕ ЧИСЛО В ДЕСЯТИЧНОЙ СИСТЕМЕ: ");
+var decimalNumber = Convert.ToInt32(Console.ReadLine());
+var binaryNumber = Change(decimalNumber);
+
+Console.WriteLine($"ЧИСЛО {decimalNumber} В ДЕСЯТИЧНОЙ СИСТЕМЕ СЧИСЛЕНИЯ - {binaryNumber}");
