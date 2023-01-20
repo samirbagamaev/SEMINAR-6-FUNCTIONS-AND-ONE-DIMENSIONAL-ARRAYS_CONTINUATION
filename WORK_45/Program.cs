2ea66ab8546d126708f1b6e37340baa5a65b2758
Samir Bagamaev <05ru4315@gmail.com>
2023-01-20 23:07:05 +0300

    WORK_45

---
+++ WORK_45/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+int[] GetRandomArray(int size, int leftRange, int rightRange) //границы диапозона
+{
+   int[] array = new int[size];
+   for(int i = 0; i < array.Length; i++)
+   {
+      array[i] = new Random().Next(leftRange, rightRange + 1); //от правого к левому 
+   }
+   return array;
+}
+
+const int size = 5;
+const int leftRange = -6;
+const int rightRange = 9;
+int[] massive = GetRandomArray(size, leftRange, rightRange);
+
+int[] newArray = new int[massive.Length];
+
+for(int i = 0; i  < massive.Length; i++)
+{
+    newArray[i] = massive[i];
+}
+
+Console.WriteLine(string.Join(",", massive));
+Console.WriteLine(string.Join(",", newArray));
+
+
