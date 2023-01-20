ebf4b46874be7885b16b7bd04df89ae6391bfde8
Samir Bagamaev <05ru4315@gmail.com>
2023-01-20 22:28:05 +0300

    WORK_44

---
+++ WORK_44/Program.cs
@@ -0,0 +1,21 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] Febonachi (int size)
+{
+    int[] Array = new int [size];
+    Array[0] = 0;
+    Array[1] = 1;
+
+    for(int i = 2; i < size; i++)
+   {
+      Array[i] = Array[i - 1] + Array[i - 2];
+   }
+   return Array;
+}
+
+int[] massive = Febonachi(55);
+Console.WriteLine(string.Join(",", massive));
